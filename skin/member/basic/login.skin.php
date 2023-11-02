d56ab842bfbb695e8a9b389c6f03bcd41bab9576
Jaegyeong Lee <wkkworud304@naver.com>
2023-11-02 16:29:12 +0900

    Add intro.css stylesheet to login.skin

--- skin/member/basic/login.skin.php
+++ skin/member/basic/login.skin.php
@@ -2,7 +2,8 @@
 if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 
 // add_stylesheet('css 구문', 출력순서); 숫자가 작을 수록 먼저 출력됨
-add_stylesheet('<link rel="stylesheet" href="'.$member_skin_url.'/style.css">', 0);
+add_stylesheet('<link rel="stylesheet" href="'.$member_skin_url.'/css/intro.css">', 0);
+add_stylesheet('<link rel="stylesheet" href="'.$member_skin_url.'/style.css">', 1);
 ?>
 
 <div class="intro_wrap" id="intro_pop">
