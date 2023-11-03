1cccd55fa5b99648903080763fc3a77d9f0834de
Jaegyeong Lee <wkkworud304@naver.com>
2023-11-03 12:13:01 +0900

    Docs: login.skin 수정

--- skin/member/basic/login.skin.php
+++ skin/member/basic/login.skin.php
@@ -3,7 +3,8 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 
 // add_stylesheet('css 구문', 출력순서); 숫자가 작을 수록 먼저 출력됨
 add_stylesheet('<link rel="stylesheet" href="'.$member_skin_url.'/css/intro.css">', 0);
-add_stylesheet('<link rel="stylesheet" href="'.$member_skin_url.'/style.css">', 1);
+add_stylesheet('<link rel="stylesheet" href="'.$member_skin_url.'/css/login.css">', 1);
+add_stylesheet('<link rel="stylesheet" href="'.$member_skin_url.'/style.css">', 2);
 ?>
 
 <script>
@@ -15,11 +16,11 @@ $(document).ready(function(){
 <div class="intro_wrap" id="intro_pop">
     <div class="intro_wrap_in">
         <div class="intro_tt_box">
-            <img src="/skin/member/basic/img/intro_logo.png" alt="고운미소로고">
+            <img src="/skin/member/basic/img/logo.png" alt="logo">
             <p class="text">안전하고 믿을 수 있는<br><strong>분야별 전문 의료진</strong></p>
         </div> 
         <div class="intro_logo2">
-            <img src="/skin/member/basic/img/intro_logo2.png" alt="연세고운미소 삼성점">
+            <img src="/skin/member/basic/img/logo2.png" alt="연세고운미소 삼성점">
         </div>
     </div>
 </div>
@@ -29,50 +30,36 @@ $(document).ready(function(){
     <div class="mbskin_box">
         <h1><?php echo $g5['title'] ?></h1>
         <div class="mb_log_cate">
-            <h2><span class="sound_only">회원</span>로그인</h2>
-            <a href="<?php echo G5_BBS_URL ?>/register.php" class="join">회원가입</a>
+            <h2 class="login_logo">
+                <img src="/skin/member/basic/img/logo.png" alt="logo">
+            </h2>
         </div>
         <form name="flogin" action="<?php echo $login_action_url ?>" onsubmit="return flogin_submit(this);" method="post">
-        <input type="hidden" name="url" value="<?php echo $login_url ?>">
-        
-        <fieldset id="login_fs">
-            <legend>회원로그인</legend>
-            <label for="login_id" class="sound_only">회원아이디<strong class="sound_only"> 필수</strong></label>
-            <input type="text" name="mb_id" id="login_id" required class="frm_input required" size="20" maxLength="20" placeholder="아이디">
-            <label for="login_pw" class="sound_only">비밀번호<strong class="sound_only"> 필수</strong></label>
-            <input type="password" name="mb_password" id="login_pw" required class="frm_input required" size="20" maxLength="20" placeholder="비밀번호">
-            <button type="submit" class="btn_submit">로그인</button>
-            
-            <div id="login_info">
-                <div class="login_if_auto chk_box">
-                    <input type="checkbox" name="auto_login" id="login_auto_login" class="selec_chk">
-                    <label for="login_auto_login"><span></span> 자동로그인</label>  
-                </div>
-                <div class="login_if_lpl">
-                    <a href="<?php echo G5_BBS_URL ?>/password_lost.php" target="_blank" id="login_password_lost">정보찾기</a>  
-                </div>
-            </div>
-        </fieldset> 
+            <input type="hidden" name="url" value="<?php echo $login_url ?>">
+            <fieldset id="login_fs">
+                <legend>회원로그인</legend>
+                <label for="login_id" class="sound_only">회원아이디<strong class="sound_only"> 필수</strong></label>
+                <input type="text" name="mb_id" id="login_id" required class="frm_input required" size="20" maxLength="20" placeholder="차트번호">
+                <label for="login_pw" class="sound_only">비밀번호<strong class="sound_only"> 필수</strong></label>
+                <input type="password" name="mb_password" id="login_pw" required class="frm_input required" size="20" maxLength="20" placeholder="전화번호">
+                <button type="submit" class="btn_submit">로그인</button>
+            </fieldset> 
         </form>
-        <?php @include_once(get_social_skin_path().'/social_login.skin.php'); // 소셜로그인 사용시 소셜로그인 버튼 ?>
+        <div class="login_logo2">
+            <a href="/">
+                <img src="/skin/member/basic/img/logo2.png" alt="연세고운미소 삼성점">
+            </a>
+        </div>
     </div>
 </div>
 
 <script>
-jQuery(function($){
-    $("#login_auto_login").click(function(){
-        if (this.checked) {
-            this.checked = confirm("자동로그인을 사용하시면 다음부터 회원아이디와 비밀번호를 입력하실 필요가 없습니다.\n\n공공장소에서는 개인정보가 유출될 수 있으니 사용을 자제하여 주십시오.\n\n자동로그인을 사용하시겠습니까?");
+    function flogin_submit(f)
+    {
+        if( $( document.body ).triggerHandler( 'login_sumit', [f, 'flogin'] ) !== false ){
+            return true;
         }
-    });
-});
-
-function flogin_submit(f)
-{
-    if( $( document.body ).triggerHandler( 'login_sumit', [f, 'flogin'] ) !== false ){
-        return true;
+        return false;
     }
-    return false;
-}
 </script>
 <!-- } 로그인 끝 -->
