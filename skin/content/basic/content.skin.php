555e0786c5d0fd7cd49634a55cb74e0624be8f50
Jaegyeong Lee <wkkworud304@naver.com>
2023-11-10 11:31:51 +0900

    Feat: privacy header 추가

--- skin/content/basic/content.skin.php
+++ skin/content/basic/content.skin.php
@@ -5,6 +5,13 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 add_stylesheet('<link rel="stylesheet" href="'.$content_skin_url.'/style.css">', 0);
 ?>
 
+<!-- s:header -->
+<div class="header">
+    <div class="logo">
+        <a href="<?=G5_URL;?>"><img src="<?=$member_skin_url;?>/img/logo2.png" alt="logo"></a>
+    </div>
+</div>
+<!-- e:header -->
 <article id="ctt" class="ctt_<?php echo $co_id; ?>">
     <header>
         <h1><?php echo $g5['title']; ?></h1>
